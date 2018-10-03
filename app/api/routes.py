9e936144624a8649dfb8e2b81f377acca551f520
ben.adams <busforce@gmail.com>
2018-10-02 22:07:43 -0500

    fixed api route added token protected routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,16 +1,52 @@
-from flask import jsonify, request
+from flask import jsonify, request, current_app
+from functools import wraps
+from datetime import datetime, timedelta
 import jwt
 from app.api import bp
 from app.models import User
 from app import db
 
+
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registeration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            user = User.query.filter_by(email=data['sub']).first()
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
+
+
 @bp.route('/hello/<string:name>/')
 def say_hello(name):
-    response = { 'msg': "Hello {}".format(name) }
+    response = {'msg': "Hello {}".format(name)}
     return jsonify(response)
 
 
-@bp.route('/register', methods=('POST',))
+@bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
     user = User(**data)
@@ -19,17 +55,24 @@ def register():
     return jsonify(user.to_dict()), 201
 
 
-@api.route('/login/', methods=('POST',))
+@bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     user = User.authenticate(**data)
 
     if not user:
-        return jsonify({ 'message': 'Invalid credentials', 'authenticated': False }), 401
+        return jsonify({'message': 'Invalid credentials', 'authenticated': False}), 401
 
     token = jwt.encode({
         'sub': user.email,
-        'iat':datetime.utcnow(),
+        'iat': datetime.utcnow(),
         'exp': datetime.utcnow() + timedelta(minutes=30)},
         current_app.config['SECRET_KEY'])
-    return jsonify({ 'token': token.decode('UTF-8') })
+    return jsonify({'token': token.decode('UTF-8')})
+
+
+@bp.route('/req')
+@token_required
+def req(current_user):
+    resp = {'msg': f"Token working {current_user}"}
+    return jsonify(resp)
